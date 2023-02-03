4b4dad81611d24e893859ba151b032a472e2f698
Konstantin Kirchev <konstantin1230@yahoo.com>
2023-02-03 20:59:27 +0200

    Add read and create dtos

---
+++ PlatformService/Dtos/PlatformReadDto.cs
@@ -0,0 +1,13 @@
+namespace PlatformService.Dtos
+{
+    public class PlatformReadDto
+    {
+        public int MyProperty { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Publisher { get; set; }
+
+        public string? Cost { get; set; }
+    }
+}
