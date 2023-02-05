c515f81977c1b5411f52aa0dbf25232be624e129
Konstantin Kirchev <konstantin1230@yahoo.com>
2023-02-05 13:55:03 +0200

    Use different DBs depending on the env

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -6,7 +6,19 @@ using PlatformService.SyncDataservices.Http;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMem"));
+if (builder.Environment.IsDevelopment())
+{
+    Console.WriteLine("--> Using InMem Db");
+    builder.Services.AddDbContext<AppDbContext>(options => 
+        options.UseInMemoryDatabase("InMem"));
+}
+else
+{
+    Console.WriteLine("--> Using SqlServer Db");
+    builder.Services.AddDbContext<AppDbContext>(options => 
+        options.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConn")));
+}
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
