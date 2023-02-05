90163b4811e7e9f2bb6aa76945a6371ca5670032
Konstantin Kirchev <konstantin1230@yahoo.com>
2023-02-05 17:17:53 +0200

    Change the route and add new method

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,18 +1,30 @@
+using CommandsService.Dtos;
+using CommandsService.Models;
+using CommandsService.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
 {
-    [Route("api/commands/[controller]")]
+    [Route("api/c/[controller]")]
     [ApiController]
     public class PlatformsController : ControllerBase
     {
-        public PlatformsController()
+        private readonly ICommandService _commandService;
+
+        public PlatformsController(ICommandService commandService)
         {
-            
+            _commandService = commandService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetAllPlatforms() 
+        {
+            return Ok(_commandService.GetAllPlatforms());
         }
 
         [HttpPost]
-        public ActionResult TestInboundConnection(){
+        public ActionResult TestInboundConnection()
+        {
             Console.WriteLine("--> Inbound POST # Command Service");
             return Ok("Inbound test from Platforms Controller");
         }
