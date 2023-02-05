58af2c1d2ea5c5c082a66821db7138166e339a06
Konstantin Kirchev <konstantin1230@yahoo.com>
2023-02-05 19:48:20 +0200

    Add commands controller

---
+++ CommandsService/Controllers/CommandsController.cs
@@ -0,0 +1,49 @@
+using CommandsService.Dtos;
+using CommandsService.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("/api/c/platforms/{platformId}/[controller]")]
+    [ApiController]
+    public class CommandsController : BaseController
+    {
+        public CommandsController(ICommandService commandService) 
+            : base(commandService)
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CommandReadDto>> GetCommandsForPlatform (int platformId) 
+        {
+            if (!_commandService.PlatformExist(platformId))
+                return NotFound();
+
+            return Ok(_commandService.GetCommandsForPlatform(platformId));
+        }
+
+        [HttpGet("{commandId: int}", Name="GetCommandForPlatform")]
+        public ActionResult<CommandReadDto> GetCommandForPlatform (int platformId, int commandId) 
+        {
+            if (!_commandService.PlatformExist(platformId))
+                return NotFound();
+                
+            var command = _commandService.GetCommand(platformId, commandId);
+
+            if (command == null) 
+                return NotFound();
+            
+            return Ok(command);
+        }
+
+        [HttpPost]
+        public ActionResult<CommandReadDto> CreateCommandForPlatform (int platformId, CommandCreateDto commandDto) {
+            if (!_commandService.PlatformExist(platformId))
+                return NotFound();
+            
+            var command = _commandService.CreateCommand(platformId, commandDto);
+            
+            return CreatedAtRoute(nameof(CreateCommandForPlatform), new {platformId = platformId, commandId = command.Id} , command);
+        }
+    }
+}
