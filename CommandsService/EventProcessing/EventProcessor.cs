b7278545f5b01d3a0f8b213cc515693ebba689b0
Konstantin Kirchev <konstantin1230@yahoo.com>
2023-02-06 13:37:51 +0200

    Add event processor

---
+++ CommandsService/EventProcessing/EventProcessor.cs
@@ -0,0 +1,84 @@
+using System.Text.Json;
+using AutoMapper;
+using CommandsService.Dtos;
+using CommandsService.Models;
+using CommandsService.Services;
+
+namespace CommandsService.EventProcessing
+{
+    public class EventProcessor : IEventProcessor
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly IMapper _mapper;
+
+        public EventProcessor(IServiceScopeFactory scopeFactory, IMapper mapper)
+        {
+            _scopeFactory = scopeFactory;
+            _mapper = mapper;
+        }
+
+        public void ProcessEvent(string message)
+        {
+            var eventType = DetermineEvent(message);
+
+            switch (eventType)
+            {
+                case EventType.PlatformPublished:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private EventType DetermineEvent (string notificationMessage)
+        {
+            Console.WriteLine("--> Determining Event");
+
+            var eventType = JsonSerializer.Deserialize<GenericEventDto>(notificationMessage);
+
+            switch (eventType.Event)
+            {
+                case "Platform_Published":
+                    Console.WriteLine("--> Platform Published Event Detected");
+                    return EventType.PlatformPublished;
+                default:
+                    Console.WriteLine("--> Could not determine event type");
+                    return EventType.Undetermined;
+            }
+
+        }
+
+        private void addPlatform(string platformPublishedMessage)
+        {
+            using(var scope = _scopeFactory.CreateScope())
+            {
+                var commandService = scope.ServiceProvider.GetRequiredService<ICommandService>();
+
+                var platformPublishedDto = JsonSerializer.Deserialize<PlatformPublishedDto>(platformPublishedMessage);
+
+                try
+                {
+                    var platform = _mapper.Map<Platform>(platformPublishedDto);
+                    if (!commandService.ExternalPlatformExist(platform.ExternalId)) 
+                    {
+                        commandService.CreatePlatform(platform);
+                    }
+                    else
+                    {
+                        Console.WriteLine("--> Platform already exists...");
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    Console.WriteLine($"--> Could not add platform to Db: {ex.Message}");
+                }
+            }
+        }
+    }
+
+    public enum EventType
+    {
+        PlatformPublished,
+        Undetermined
+    }
+}
